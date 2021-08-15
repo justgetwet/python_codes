805b6ea89f512fe6944d80d99bca53815b4531b4
AkiraWin <frog7bar@gmail.com>
2021-08-15 16:53:14 +0900

    add post

--- docs/_posts/2021-06-05-tkdataframe.md
+++ docs/_posts/2021-06-05-tkdataframe.md
@@ -12,83 +12,104 @@ import pandas as pd
 import seaborn as sns
 import unicodedata
 
-def main():
-	pass
-
-def set_table(frame):
+class TkSample:
   
-  iris = sns.load_dataset('iris')
-  df = iris.head(6)
-  headingcolor = "lightgrey"
-  alternatecolor = "whitesmoke"
+  def __init__(self, title: str, df: pd.DataFrame):
+    self.root = tk.Tk()
+    self.root.title("tk sample")
+    self.root.geometry("500x250+200+50")
 
-  tree = ttk.Treeview(frame, height=len(df)+1)
+    self.title = title
+    self.df = df
 
-  def fixed_map(option):
-    # Fix for setting text colour for Tkinter 8.6.9
-    # From: https://core.tcl.tk/tk/info/509cafafae
-    #
-    # Returns the style map for 'option' with any styles starting with
-    # ('!disabled', '!selected', ...) filtered out.
-    #
-    # style.map() returns an empty list for missing options, so this
-    # should be future-safe.
-    return [elm for elm in style.map('Treeview', query_opt=option) if elm[:2] != ('!disabled', '!selected')]	
-  
-  style = ttk.Style()
-  style.theme_use("default")
-  style.configure("Treeview.Heading", background=headingcolor)
-  style.map('Treeview', foreground=fixed_map('foreground'), background=fixed_map('background'))
-  
-  tree["show"] = "headings"
-  cols = tuple(range(1, len(df.columns)+1))
-  tree['columns'] = cols
+    self.frame = tk.Frame(self.root)
 
-  sizes = column_sizes(df)
-  for i, col, size in zip(cols, df.columns, sizes):
-    tree.heading(i, text=f"{col}")
-    tree.column(i, width=size+8) # 
-  
-  lst = [tuple(t)[1:] for t in df.itertuples()]
-  for i, tpl in enumerate(lst):
-    tree.insert("", "end", tags=i, values=tpl)
-    if i & 1:
-      tree.tag_configure(i, background=alternatecolor)
-  
-  tree.pack(pady=10, padx=10)
+  def set_toolbar_frame(self):
+    f_toolbar = tk.Frame(self.frame, bg="whitesmoke", height=50, width=500, pady=10, padx=10)
+    f_toolbar.pack(fill=tk.X)
+    
+    l_title = tk.Label(f_toolbar, text=self.title, bg="whitesmoke", anchor="w")
+    l_title.pack(side=tk.LEFT, expand=True, anchor=tk.W)
 
-def column_sizes(df: pd.DataFrame) -> list:
+    b_quit = ttk.Button(f_toolbar, text='Quit', command=lambda: root.quit())
+    b_quit.pack(side=tk.LEFT, expand=True, anchor=tk.E)
 
-  def ea_width_count(text):
-    count = 0
-    for c in text:
-      if unicodedata.east_asian_width(c) in 'FWA':
-        count += 2
-      else:
-        count += 1
-    return count * 8
-  
-  lst_columns = [[col] + list(df[col]) for col in df.columns]
-  col_sizes = []
-  for cols in lst_columns:
-    col_sizes.append(max([ea_width_count(str(e)) for e in cols]))
 
-  return col_sizes
+  def set_table_on_frame(self):
+    f_table = tk.Frame(self.frame, height=200, width=500, pady=10, padx=10)
+    
+    headingcolor = "lightgrey"
+    alternatecolor = "whitesmoke"
 
+    tree = ttk.Treeview(f_table, height=len(df)+1)
+
+    def fixed_map(option):
+      # Fix for setting text colour for Tkinter 8.6.9
+      # From: https://core.tcl.tk/tk/info/509cafafae
+      #
+      # From: https://ja.stackoverflow.com/questions/64095/
+      # Python ttk.Treeview python3.7でリストに割り当てたtagに対して色を設定する方法
+      return [elm for elm in style.map('Treeview', query_opt=option) if elm[:2] != ('!disabled', '!selected')]	
+    
+    style = ttk.Style()
+    style.theme_use("default")
+    style.configure("Treeview.Heading", background=headingcolor)
+    style.map('Treeview', foreground=fixed_map('foreground'), background=fixed_map('background'))
+    
+    tree["show"] = "headings"
+    cols = tuple(range(1, len(self.df.columns)+1))
+    tree['columns'] = cols
+
+    sizes = self.column_sizes()
+    for i, col, size in zip(cols, self.df.columns, sizes):
+      tree.heading(i, text=f"{col}")
+      tree.column(i, width=size+8)
+    
+    tpls = [tuple(t)[1:] for t in self.df.itertuples()]
+    for i, tpl in enumerate(tpls):
+      tree.insert("", "end", tags=i, values=tpl)
+      if i & 1:
+        tree.tag_configure(i, background=alternatecolor)
+    
+    tree.pack(pady=10, padx=10)
+    f_table.pack(fill=tk.BOTH)
+
+  def column_sizes(self) -> list:
+
+    def east_asian_width_count(text):
+      count = 0
+      for c in text:
+        if unicodedata.east_asian_width(c) in 'FWA':
+          count += 2
+        else:
+          count += 1
+      return count * 8
+    
+    lst_columns = [[col] + list(df[col]) for col in self.df.columns]
+    sizes = []
+    for cols in lst_columns:
+      max_size = max([east_asian_width_count(str(col)) for col in cols])
+      sizes.append(max_size)
+
+    return sizes
+
+  def run(self):
+    self.set_toolbar_frame()
+    self.set_table_on_frame()
+    self.frame.pack()
+    self.root.mainloop()
 ```
 
 usage
 
 ```python
-def main():
-	# 略
-
-  f_main = tk.Frame(root, height=200, width=500, pady=10, padx=10)
-  set_table(f_main)
-  f_main.pack(fill=tk.BOTH)
+if __name__ == '__main__':
   
-  root.mainloop()
-
+  title = 'iris data'
+  iris = sns.load_dataset('iris')
+  df = iris.head(6)
+  t = TkSample(title, df)
+  t.run()
 ```
 
 run!
--- tksample.py
+++ tksample.py
@@ -4,93 +4,98 @@ import pandas as pd
 import seaborn as sns
 import unicodedata
 
-def main():
-  root = tk.Tk()
-  root.title("tk sample")
-
-  root.geometry("500x250")
-
-  frame1(root)
+class TkSample:
   
-  root.mainloop()
+  def __init__(self, title: str, df: pd.DataFrame):
+    self.root = tk.Tk()
+    self.root.title("tk sample")
+    self.root.geometry("500x250+200+50")
 
-def frame1(root):
+    self.title = title
+    self.df = df
 
-  Frame = tk.Frame(root)
+    self.frame = tk.Frame(self.root)
 
-  table_title = "this is title."
-  f_toolbar = tk.Frame(Frame, bg="whitesmoke", height=50, width=500, pady=10, padx=10)
-  f_toolbar.pack(fill=tk.X)
-  
-  l_title = tk.Label(f_toolbar, text=table_title, bg="whitesmoke", anchor="w")
-  l_title.pack(side=tk.LEFT, expand=True, anchor=tk.W)
+  def set_toolbar_frame(self):
+    f_toolbar = tk.Frame(self.frame, bg="whitesmoke", height=50, width=500, pady=10, padx=10)
+    f_toolbar.pack(fill=tk.X)
+    
+    l_title = tk.Label(f_toolbar, text=self.title, bg="whitesmoke", anchor="w")
+    l_title.pack(side=tk.LEFT, expand=True, anchor=tk.W)
 
-  b_quit = ttk.Button(f_toolbar, text='Quit', command=lambda: root.quit())
-  b_quit.pack(side=tk.LEFT, expand=True, anchor=tk.E)
+    b_quit = ttk.Button(f_toolbar, text='Quit', command=lambda: root.quit())
+    b_quit.pack(side=tk.LEFT, expand=True, anchor=tk.E)
 
-  f_main = tk.Frame(Frame, height=200, width=500, pady=10, padx=10)
-  set_table(f_main)
-  f_main.pack(fill=tk.BOTH)
 
-  Frame.pack()
+  def set_table_on_frame(self):
+    f_table = tk.Frame(self.frame, height=200, width=500, pady=10, padx=10)
+    
+    headingcolor = "lightgrey"
+    alternatecolor = "whitesmoke"
 
-def set_table(frame):
-  
-  iris = sns.load_dataset('iris')
-  df = iris.head(6)
-  headingcolor = "lightgrey"
-  alternatecolor = "whitesmoke"
+    tree = ttk.Treeview(f_table, height=len(df)+1)
 
-  tree = ttk.Treeview(frame, height=len(df)+1)
+    def fixed_map(option):
+      # Fix for setting text colour for Tkinter 8.6.9
+      # From: https://core.tcl.tk/tk/info/509cafafae
+      #
+      # From: https://ja.stackoverflow.com/questions/64095/
+      # Python ttk.Treeview python3.7でリストに割り当てたtagに対して色を設定する方法
+      return [elm for elm in style.map('Treeview', query_opt=option) if elm[:2] != ('!disabled', '!selected')]	
+    
+    style = ttk.Style()
+    style.theme_use("default")
+    style.configure("Treeview.Heading", background=headingcolor)
+    style.map('Treeview', foreground=fixed_map('foreground'), background=fixed_map('background'))
+    
+    tree["show"] = "headings"
+    cols = tuple(range(1, len(self.df.columns)+1))
+    tree['columns'] = cols
 
-  def fixed_map(option):
-    # Fix for setting text colour for Tkinter 8.6.9
-    # From: https://core.tcl.tk/tk/info/509cafafae
-    #
-    # From: https://ja.stackoverflow.com/questions/64095/
-    # Python ttk.Treeview python3.7でリストに割り当てたtagに対して色を設定する方法
-    return [elm for elm in style.map('Treeview', query_opt=option) if elm[:2] != ('!disabled', '!selected')]	
-  
-  style = ttk.Style()
-  style.theme_use("default")
-  style.configure("Treeview.Heading", background=headingcolor)
-  style.map('Treeview', foreground=fixed_map('foreground'), background=fixed_map('background'))
-  
-  tree["show"] = "headings"
-  cols = tuple(range(1, len(df.columns)+1))
-  tree['columns'] = cols
+    sizes = self.column_sizes()
+    for i, col, size in zip(cols, self.df.columns, sizes):
+      tree.heading(i, text=f"{col}")
+      tree.column(i, width=size+8) # 
+    
+    tpls = [tuple(t)[1:] for t in self.df.itertuples()]
+    for i, tpl in enumerate(tpls):
+      tree.insert("", "end", tags=i, values=tpl)
+      if i & 1:
+        tree.tag_configure(i, background=alternatecolor)
+    
+    tree.pack(pady=10, padx=10)
+    f_table.pack(fill=tk.BOTH)
 
-  sizes = column_sizes(df)
-  for i, col, size in zip(cols, df.columns, sizes):
-    tree.heading(i, text=f"{col}")
-    tree.column(i, width=size+8) # 
-  
-  lst = [tuple(t)[1:] for t in df.itertuples()]
-  for i, tpl in enumerate(lst):
-    tree.insert("", "end", tags=i, values=tpl)
-    if i & 1:
-      tree.tag_configure(i, background=alternatecolor)
-  
-  tree.pack(pady=10, padx=10)
+  def column_sizes(self) -> list:
 
-def column_sizes(df: pd.DataFrame) -> list:
+    def east_asian_width_count(text):
+      count = 0
+      for c in text:
+        if unicodedata.east_asian_width(c) in 'FWA':
+          count += 2
+        else:
+          count += 1
+      return count * 8
+    
+    lst_columns = [[col] + list(df[col]) for col in self.df.columns]
+    sizes = []
+    for cols in lst_columns:
+      max_size = max([east_asian_width_count(str(col)) for col in cols])
+      sizes.append(max_size)
 
-  def ea_width_count(text):
-    count = 0
-    for c in text:
-      if unicodedata.east_asian_width(c) in 'FWA':
-        count += 2
-      else:
-        count += 1
-    return count * 8
-  
-  lst_columns = [[col] + list(df[col]) for col in df.columns]
-  col_sizes = []
-  for cols in lst_columns:
-    col_sizes.append(max([ea_width_count(str(e)) for e in cols]))
+    return sizes
 
-  return col_sizes
+  def run(self):
+    self.set_toolbar_frame()
+    self.set_table_on_frame()
+    self.frame.pack()
+    self.root.mainloop()
 
 if __name__ == '__main__':
-
-  main()
+  
+  title = 'iris data'
+  iris = sns.load_dataset('iris')
+  df = iris.head(6)
+  
+  t = TkSample(title, df)
+  t.run()
